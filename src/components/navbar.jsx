27acacefca33fe35adf0a2419d20f175b27bd71d
xxRohitxx <rohitrs319791@gmail.com>
2023-02-05 23:55:31 +0530

    loading indicator

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 
 import "./navbar.css"
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
 export default function Navbar(){
     const [visible,setvisible]=useState(false);
 
@@ -24,8 +25,8 @@ return()=>window.removeEventListener("scroll",NavbarAnimation)
     return(
         <div className={`navbar ${visible && "black_bg"}`} >
             <div className="nav_container">
-            <img className="nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
-
+            <Link to="/"><img className="nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+</Link>
 
 <img className="nav_avatar" src="https://i.pinimg.com/736x/db/70/dc/db70dc468af8c93749d1f587d74dcb08.jpg" alt="" />
             </div>
--- src/pages/trailerPage.jsx
+++ src/pages/trailerPage.jsx
@@ -1,37 +1,46 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Routes, Route, useParams } from 'react-router-dom';
 import Navbar from '../components/navbar';
 import requests from '../functions/request';
 import Slide from '../components/Slide';
+import axios from 'axios';
 
 const TrailerPage=()=>{
-const [videoId,setvideoId]=useState("JOddp-nlNvQ")
+const [videoId,setvideoId]=useState("")
 const {query}=useParams()
+const key="AIzaSyBPYuyh4hQv7MgT5Hp1nFoMcihRrilJn5E"
 
+async function fetchdata(name){
+try{
+axios.get(`https://youtube.googleapis.com/youtube/v3/search?q=${name}&key=${key}&part=snippet&maxResults=1`).then((res)=>{
+   let resvideoID=res.data.items[0].id.videoId
+setvideoId(resvideoID)
+})
+}catch(err){
 
+}
+}
 
 useEffect(() => {
-   fetchdata(query)
+  
 }, [videoId]);
 
 return(
     <div>
       <Navbar/>
 
-<div style={{height:"100vh",paddingTop:"2%",}}>
 
-<div style={{display:"flex" ,height:"60%" }}>
-        <iframe style={{margin:"auto"}} width="90%" height="90%" allow='autoPlay'  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`} autoplay={1} 
-   allowFullScreen="true"    frameborder="0"></iframe>
+<div style={{height:"100vh",paddingTop:"2%",display:"flex" }}>
+      
+<div style={{margin:"auto",display:"flex"}}>
+
+<img width="200" src="https://i.ibb.co/mJ0mqDW/Rolling-1s-200px.gif" alt="Spinner-1-8s-243px" border="0" />
+</div>
 
 
       </div>
-      
-<Slide
-        title="MORE LIKE THIS"
-        fetchUrl={requests.fetchNetflixOriginals}
-      />
-</div>
+
+
       
 
      
